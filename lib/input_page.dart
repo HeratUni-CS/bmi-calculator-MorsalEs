16dfec51278fee59bc2a4616054df49e51dd7e1b
MorsalEs <eshaqzai.mary@gmail.com>
2022-11-01 23:12:45 +0430

    create input page and create reusable container

---
+++ lib/input_page.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import './constants.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import './icon_content.dart';
+import './reusable_card.dart';
+
+enum Gender { male, female }
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? selectedGender;
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  onPress: () {
+                    setState(() {
+                      selectedGender = Gender.male;
+                    });
+                  },
+                  color: selectedGender == Gender.male
+                      ? kActiveCardColor
+                      : kInactiveCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MALE',
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  onPress: () {
+                    setState(() {
+                      selectedGender = Gender.female;
+                    });
+                  },
+                  color: selectedGender == Gender.female
+                      ? kActiveCardColor
+                      : kInactiveCardColor,
+                  child: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              child: Container()
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: [
+              Expanded(
+                child: ReusableCard(
+                  child: Container(),
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  child: Container(),
+                ),
+              )
+            ],
+          )),
+          Container(
+            child: Center(
+              child: Text(
+                "CALCULATE",
+                style: kLargeButtonTextStyle,
+              ),
+            ),
+            color: kBottomContainerColor,
+            margin: EdgeInsets.only(top: 10),
+            width: double.infinity,
+            height: kBottomContainerHeight,
+          ),
+        ],
+      ),
+    );
+  }
+}
